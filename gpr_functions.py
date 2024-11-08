711928cb509f755850cc2c8ec9d2524b249dc057
callumrjohn <callumrjohn@gmail.com>
2024-11-08 11:57:29 +0000

    modifications to old code

--- gpr_functions.py
+++ gpr_functions.py
@@ -15,6 +15,10 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel, RBF, Matern, ConstantKernel as C, RationalQuadratic, ExpSineSquared
 
+import warnings
+
+warnings.filterwarnings('ignore')
+
 
 '''Function to return a set of predicitions, with errors, 
 for a set of compound data using a dictionary of kernels.
@@ -33,20 +37,23 @@ def get_predictions(X,
                     ):
 
     results = []
-    print(X)
-    print(y)
+    #print(X)
+    #print(y)
     x = np.linspace(np.min(X.flatten()), np.max(X.flatten()), 1000).reshape(-1, 1)
 
     for kernel_name, kernel in kernels.items():
-
+        
+        #run the GPR model
         model = GaussianProcessRegressor(kernel = kernel, n_restarts_optimizer = 7)
         pipe = Pipeline([('scaler', scaler), ('model', model)])
         pipe.fit(X, y)
         y_pred, y_pred_std = pipe.predict(x, return_std=True)
        
-        print(x)
-        print(y_pred)
-        print(y_pred.ravel())
+        #print(x)
+        #print(y_pred)
+        #print(y_pred.ravel())
+
+
         # Calculate maximum peak height
 
         peak_index, _ = find_peaks(y_pred.ravel(), height = 5)
@@ -76,12 +83,18 @@ def get_predictions(X,
             abs_error = -abs(pipe.predict(X_test) - y_test)
             neg_aes.append(abs_error[0])
         
-        print(neg_aes)
+        #print(neg_aes)
         mae = -mean(neg_aes)
 
 
 
-        model_results = {'kernel' : kernel_name, 'optimum': (x_peak, y_peak), 'predictions' : (x.flatten(), y_pred), 'pred_std' : y_pred_std, 'mae' : mae}
+        model_results = {'kernel' : kernel_name, 
+                         'optimum': (x_peak, y_peak), 
+                         'predictions' : (x.flatten(), y_pred), 
+                         'pred_std' : y_pred_std, 
+                         'mae' : mae,
+                         'exp_data' : (X.flatten(), y),
+                         }
 
         results.append(model_results)
             
@@ -90,10 +103,9 @@ def get_predictions(X,
 
 
 def model_selector(model_results):
-    print(model_results[0]['mae'])
+    #print(model_results[0]['mae'])
     best_model_dic = min(model_results, key = lambda x: x['mae'], default=None)
-    #best_model = best_model_dic.values()[0]
-    #best_model.update({'kernel_name' : best_model_dic.keys()[0]})
+
     return best_model_dic
 
 
@@ -102,7 +114,7 @@ def plot_predicitions(data,  # list of dictionaries
                       xlabel, 
                       ylabel):
     
-    no_comp = len(set([x['compound'].split(' ')[0] for x in data])) #get the number of starting materials, so the dimentions are correct when plotting multiple products
+    #no_comp = len(set([x['compound'].split(' ')[0] for x in data])) #get the number of starting materials, so the dimentions are correct when plotting multiple products
 
     #fig, ax = plt.subplots(math.ceil(no_comp/3), 3, figsize = (15,15))
 
@@ -110,14 +122,45 @@ def plot_predicitions(data,  # list of dictionaries
 
     color_count = 0
     colors = ['blue','red','green']
+    marker_colors = ['darkblue','darkred','darkgreen']
 
     for i, comp_data in enumerate(data):
 
         x = comp_data['predictions'][0]
         y = comp_data['predictions'][1]
+
+        x_exp = comp_data['exp_data'][0]
+        y_exp = comp_data['exp_data'][1]
+
         stdev = comp_data['pred_std']
 
-        plt.plot(x, y, color = colors[color_count])
+        plt.plot(x, 
+                 y, 
+                 color = colors[color_count],
+                 )
+
+
+        plt.scatter(x_exp,
+                    y_exp,
+                    color = marker_colors[color_count],
+                    marker= 'x',
+                    s = 14,
+                    )
+
+        upper_bound = y + 1.9600 * stdev
+        lower_bound = y - 1.9600 * stdev
+        
+        plt.fill_between(x, 
+                         upper_bound, 
+                         lower_bound, 
+                         alpha = .15, 
+                         fc = colors[color_count], 
+                         ec = 'None',
+                         )
+        plt.xlim(0, 25)
+        plt.ylim(0, 100)
+
+    
 
         upper_bound = y + 1.9600 * stdev
         lower_bound = y - 1.9600 * stdev
@@ -129,10 +172,10 @@ def plot_predicitions(data,  # list of dictionaries
         #         ec='None')
 
         if i == len(data) - 1 or data[i]['compound'].split(' ')[0] != data[i+1]['compound'].split(' ')[0]:
-            plt.title(comp_data['compound'])
+            plt.title(comp_data['compound'].split(' ')[0])
             plt.xlabel(xlabel)
             plt.ylabel(ylabel)
-            plt.savefig(comp_data['compound'] + '_plot.png')
+            plt.savefig("plots\\" + comp_data['compound'] + '_plot.png')
             plt.clf()
             color_count = 0
 
