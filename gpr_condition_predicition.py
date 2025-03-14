ffa4c4a74facf4d82aa01700085fa04d2fd169de
callumrjohn <callumrjohn@gmail.com>
2025-03-14 17:01:19 +0000

    Added error handling and commented

--- gpr_condition_predicition.py
+++ gpr_condition_predicition.py
@@ -1,39 +1,61 @@
 import os
-import pip
 import sys
-
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.gaussian_process.kernels import RBF, Matern, RationalQuadratic, ExpSineSquared, WhiteKernel
-from sklearn.preprocessing import StandardScaler, RobustScaler
 from gpr_predictions import Halogenation
 
+import warnings
+warnings.filterwarnings("ignore") # Ignore convergence warnings when running the GPR models
 
-#load data
 
-yield_csv_path = input("Enter the path to the yield data: ").strip('"').strip("'")
-conv_csv_path = input("Enter the path to the conversion data: ").strip('"').strip("'")
+# load data and set up parameters based on user input
 
-destination = input("Enter the path to the destination folder: ").strip('"').strip("'")
+yield_csv_path = input("Enter the path to the yield data: ").strip()
+conv_csv_path = input("Enter the path to the conversion data: ").strip
 
-length_scale_bounds = input("Enter the length scale bounds in the form lower,upper: ")
+destination = input("Enter the path to the destination folder: ").strip()
+
+length_scale_bounds = input("Enter the length scale bounds in the form lower,upper (default: 0.1,10): ").strip()
 length_scale_bounds = tuple([float(x) for x in length_scale_bounds.split(',')])
 
-compound_yield_csv = pd.read_csv(yield_csv_path, index_col='Unnamed: 0')
-compound_conv_csv = pd.read_csv(conv_csv_path, index_col='Unnamed: 0')
+if len(length_scale_bounds) != 2: # Check if the user has entered valid length scale bounds
+    print("Invalid length scale bounds. Defaulting to 0.1,10")
+    length_scale_bounds = (0.1, 10)
 
+try: # Check if the files exist
+    compound_yield_csv = pd.read_csv(yield_csv_path, index_col='Unnamed: 0')
+    compound_conv_csv = pd.read_csv(conv_csv_path, index_col='Unnamed: 0')
+except FileNotFoundError as e:
+    print(f"Error: {e}")
+    sys.exit(1) # Exit if the files do not exist
+
+if compound_yield_csv.index.all() != compound_conv_csv.index.all(): # Check if substrates of the yield and conversion data files match
+    print("Error: The indices of the yield and conversion data do not match")
+    sys.exit(1) # Exit if the substrates do not match accross yield and conversion values
+
+# seperate HTE data from experimental values - comment out all lines if .csv only contians HTE data
 compound_yield= compound_yield_csv.iloc[:,:-1]
 compound_conv= compound_conv_csv.iloc[:,:-1]
-
 compound_exp_yield = compound_yield_csv.iloc[:,-1].fillna(0)
 compound_exp_conv = compound_conv_csv.iloc[:,-1].fillna(0)
 
-if not os.path.exists(destination + "\compound_plots"):
-    os.mkdir(destination + "\compound_plots")
+if not os.path.exists(os.path.join(destination, "compound_plots")): # Create a folder to store the plots
+    os.mkdir(os.path.join(destination, "compound_plots"))
 
 
-#run the model for each set of data
+#define kernels
+kernels = {'RBF': 1.0 * RBF(length_scale=1e0, length_scale_bounds=length_scale_bounds)+ WhiteKernel(noise_level=1e0, noise_level_bounds=length_scale_bounds),
+          'Matern (nu = 3/2)': 1.0 * Matern(length_scale=1e0, length_scale_bounds=length_scale_bounds, nu=3/2)+ WhiteKernel(noise_level=1e0, noise_level_bounds=length_scale_bounds),
+          'Matern (nu = 5/2)': 1.0 * Matern(length_scale=1e0, length_scale_bounds=length_scale_bounds, nu=5/2)+ WhiteKernel(noise_level=1e0, noise_level_bounds=length_scale_bounds),
+          #'Rational quadratic (RQ)': 1.0 * RationalQuadratic(length_scale=1e0, length_scale_bounds=length_scale_bounds),+ WhiteKernel(noise_level=1e0, noise_level_bounds=length_scale_bounds),
+          #'Exponential sine squared (ESS)': 1.0 * ExpSineSquared(length_scale=1e0, length_scale_bounds=length_scale_bounds),, periodicity=1.0, periodicity_bounds=length_scale_bounds),+ WhiteKernel(noise_level=1e0, noise_level_bounds=length_scale_bounds),),
+          #UNCOMMENT THE ABOVE LINES TO USE OTHER KERNELS (see README for more information)
+          }
+
+
+# initialise dataframe to store predictions
 compound_df = pd.DataFrame(columns=['pred_opt_tfa', 
                             'pred_opt_yield',
                             'pred_opt_conv',
@@ -42,77 +64,69 @@ compound_df = pd.DataFrame(columns=['pred_opt_tfa',
                             'kernel_conv',
                             'kernel_conv_error',
                             'length_scale_bounds',
-
-
                            ])
 
-# predictions
-labels = compound_yield.index
-print(labels)
-
+# initialise lists to store GPR predictions for csv export
 gpr_predictions_yield = []
 gpr_predictions_conv = []
 
-for label in labels:
+# predictions
+substrates = compound_yield.index
+
+for label in substrates: # Iterate through the substrates, run predictions and plot the data
+
+
+    # RUN THE MODEL
 
     print("Running predictions for " + label)
-
-    kernels = {'RBF': 1.0 * RBF(length_scale=1e0, length_scale_bounds=length_scale_bounds)+ WhiteKernel(noise_level=1e0, noise_level_bounds=length_scale_bounds),
-          'Matern (nu = 3/2)': 1.0 * Matern(length_scale=1e0, length_scale_bounds=length_scale_bounds, nu=3/2)+ WhiteKernel(noise_level=1e0, noise_level_bounds=length_scale_bounds),
-          'Matern (nu = 5/2)': 1.0 * Matern(length_scale=1e0, length_scale_bounds=length_scale_bounds, nu=5/2)+ WhiteKernel(noise_level=1e0, noise_level_bounds=length_scale_bounds),
-          #'Rational quadratic (RQ)': 1.0 * RationalQuadratic(length_scale=1e0, length_scale_bounds=length_scale_bounds),+ WhiteKernel(noise_level=1e0, noise_level_bounds=length_scale_bounds),
-          #'Exponential sine squared (ESS)': 1.0 * ExpSineSquared(length_scale=1e0, length_scale_bounds=length_scale_bounds),, periodicity=1.0, periodicity_bounds=length_scale_bounds),+ WhiteKernel(noise_level=1e0, noise_level_bounds=length_scale_bounds),),
-          }
 
     substrate = pd.concat([compound_yield.loc[label], compound_conv.loc[label]], axis=1)
     substrate.columns = ['Yield', 'Conversion']
     substrate = np.array(substrate.reset_index())
 
-    compound = Halogenation(label, 'X', substrate)
+    compound = Halogenation(label, 'X', substrate) # Initialise the Halogenation class
 
-    for kernel_name, kernel in kernels.items():
-        compound.gprcalculate(kernel, dset= 'both', kernelname = kernel_name)
+    for kernel_name, kernel in kernels.items(): # Iterate through the kernels and calculate the GPR predictions
+        compound.gprcalculate(kernel, dset= 'both', kernelname = kernel_name) 
     
-    yield_model = compound.best_model_yield()
-    #print(yield_model)
-    #print(type(yield_model))
-    conv_model = compound.best_model_conv()
+    yield_model = compound.best_model_yield() # Get the best kernel for yield
+    conv_model = compound.best_model_conv() # Get the best kernel for conversion
 
-    optimum_tfa = compound.pickoptimum(yield_kernel = yield_model, 
+    optimum_tfa = compound.pickoptimum(yield_kernel = yield_model, # Get the optimum TFA, yield, and conversion values using the best kernels
                                    conv_kernel = conv_model)
     
     #print(optimum_tfa)
 
-    compound_df.loc[label] = [optimum_tfa[0], 
-                              optimum_tfa[1], 
-                              optimum_tfa[2], 
-                              yield_model, 
-                              compound.yieldoutputs[yield_model]['mae'], 
-                              conv_model, 
-                              compound.convoutputs[conv_model]['mae'], 
-                              length_scale_bounds]
-    #export to csv
-    
+    # append GPR data to dataframe
+    compound_df.loc[label] = [optimum_tfa[0], # optimum TFA
+                              optimum_tfa[1], # optimum yield
+                              optimum_tfa[2], # optimum conversion
+                              yield_model, # best kernel (lowest MAE) for yield
+                              compound.yieldoutputs[yield_model]['mae'], # MAE values for all yield predictions 
+                              conv_model, # best kernel (lowest MAE) for conversion
+                              compound.convoutputs[conv_model]['mae'], # MAE values for all conversion predictions
+                              length_scale_bounds] # length scale bounds
 
-    #plotting
     
+    # PLOT THE DATA
 
+    # GPR predictions
     x = compound.yieldoutputs[yield_model]['prediction'][:,0]
     y = compound.yieldoutputs[yield_model]['prediction'][:,1]
 
-    gpr_predictions_yield.append(y)
-
     x_c = compound.convoutputs[conv_model]['prediction'][:,0]
     y_c = compound.convoutputs[conv_model]['prediction'][:,1]
-    
+
+    gpr_predictions_yield.append(y)
     gpr_predictions_conv.append(y_c)
 
-
+    # experimental data
     x_exp = compound.acidequiv
     y_exp = compound.yieldvalues
     y_exp_c = compound.convvalues
-    
 
+
+    # plot a graph of the GPR predictions and experimental yield and conversion data for each halogenation reactions, inlcuding the optimum TFA
     fig, ax = plt.subplots(1,2,figsize=(17, 5), gridspec_kw={'width_ratios': [1,1], 'wspace': 0.3})
 
     ax[0].plot(x,y, zorder = 3, color = 'tab:blue')
@@ -129,8 +143,9 @@ for label in labels:
         ax[0].set_ylim(0, 100)
         ax[0].vlines(optimum_tfa[0], colors='tab:red', linestyles='dashed', ymin=0, ymax=100, linewidth=0.5, zorder = 6)
     
+    #ax[0].set_title(f"Predictions for {label}")
     ax[0].set_xlabel('TFA equivalents')
-    ax[0].set_ylabel('NMR yield/Conversion (%)')
+    ax[0].set_ylabel('Yield/Conversion (%)')
 
     ax[0].legend(['NMR yield (GPR)', 'Conversion (GPR)','GPR Optimum', 'Isolated yield'], 
             loc='lower right', 
@@ -148,6 +163,7 @@ for label in labels:
     ax[0].text(-2.5, -23, "Conversion - Kernel: "+ compound.best_model_conv() + " + White" + ', Length scale bounds: ' + str(length_scale_bounds), fontsize=8)
         
 
+    # plot a bar graph of the best HTE conversion, predicted optimal conversion, scale-up conversion, and isolated yield for each halogenation reaction
     iso_yield = compound_exp_yield.loc[label]
     iso_conv = compound_exp_conv.loc[label]
     best_index = np.argmax(substrate[:,1])
@@ -166,14 +182,21 @@ for label in labels:
     ax[1].set_xlabel('Yield/Conversion (%)')
     ax[1].set_xlim(0, 100)
 
-    plt.savefig(destination + "\compound_plots\\" + label + "_plot.png", bbox_inches='tight')
+
+    # save the plot to the destination folder
+    plot_filename = os.path.join(destination, "compound_plots", f"{label}_plot.png")
+    plt.savefig(plot_filename, bbox_inches='tight')
 
 
 
+#export information on optimums, kernels, and yield/conversion responses to csv
+gpr_predictions_yield_df = pd.DataFrame(gpr_predictions_yield, columns = x, index=substrates)
+gpr_predictions_conv_df = pd.DataFrame(gpr_predictions_conv, columns = x, index=substrates)
 
-compound_df.to_csv(destination + "\compound_predictions.csv")
-gpr_predictions_yield_df = pd.DataFrame(gpr_predictions_yield, columns = x, index=labels)
-gpr_predictions_conv_df = pd.DataFrame(gpr_predictions_conv, columns = x, index=labels)
+compound_df_file = os.path.join(destination, "compound_predictions.csv")
+gpr_predictions_yield_file = os.path.join(destination, "gpr_predictions_yield.csv")
+gpr_predictions_conv_file = os.path.join(destination, "gpr_predictions_conv.csv")
 
-gpr_predictions_yield_df.to_csv(destination + "\gpr_predictions_yield.csv")
-gpr_predictions_conv_df.to_csv(destination + "\gpr_predictions_conv.csv")
+compound_df.to_csv(compound_df_file)
+gpr_predictions_yield_df.to_csv(gpr_predictions_yield_file)
+gpr_predictions_conv_df.to_csv(gpr_predictions_conv_file)
