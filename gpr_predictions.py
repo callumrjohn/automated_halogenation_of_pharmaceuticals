4736c21cfe8312ae85c3bed313be99d4d434001b
Callum R John <c.john21@imperial.ac.uk>
2025-03-24 11:38:48 +0000

    Further commenting

--- gpr_predictions.py
+++ gpr_predictions.py
@@ -40,8 +40,6 @@ Output: np.array containing the acid equiv, yield, and conversion (if available)
 
 '''
 
-
-
 class Halogenation:
 
     def __init__(self,
@@ -49,18 +47,18 @@ class Halogenation:
                  reagent, 
                  data, 
                  ):
-        self._substrate = substrate #str
-        self._reagent = reagent #str
+        self._substrate = substrate #str - the substrate used in the reaction
+        self._reagent = reagent #str - the reagent used in the reaction
         self._data = data #can be any 2D array with 2 or 3 columns: acid equiv, yield, and conversion (optional)
 
-        self.yieldoutputs = {}
-        if self.data.shape[1] == 3:
+        self.yieldoutputs = {} # initialise a dictionary to store the GPR predictions for yield
+        if self.data.shape[1] == 3: # initialise a dictionary to store the GPR predictions for conversion if the data has 3 columns
             self.convoutputs = {}
 
     def __str__(self):
         return "Data for the halogenation of " + self.substrate + " with " + self.reagent
     
-    #getters and setters
+    # Getters and Setters
     @property
     def substrate(self):
         return self._substrate
@@ -83,11 +81,9 @@ class Halogenation:
     def data(self):
         return np.array(self._data)
 
-
     @property
     def acidequiv(self):
         return np.array([float(x[0]) for x in self._data])
-
     
     @property
     def yieldvalues(self):
@@ -101,22 +97,19 @@ class Halogenation:
             return None
 
 
-
-
-    #method to run the GPR model. Integrate into a for loop to run multiple kernels
+    # The main method use to run the GPR model. Involved using acid equiv with yield and/or conversion values to generate model(s)  
     def gprcalculate(self, 
                       kernel, 
                       **kwargs,
                       ):
         
-        #default values for kwargs
+        # Default values for kwargs
         kernelname = kwargs.get('kernelname', str(kernel))
-        scaler = kwargs.get('scaler', RobustScaler())
-        height = kwargs.get('height', 5)
-        dset = kwargs.get('dset', 'yield')
-
+        scaler = kwargs.get('scaler', RobustScaler()) # Default scaler, generally better at handling outliers than StandardScaler
+        height = kwargs.get('height', 5) # Only pick peaks above 5% yield/conversion. Change accordingly
+        dset = kwargs.get('dset', 'yield') # Use yield data only by default
        
-        X = self.acidequiv.reshape(-1, 1) #reshape the acid equiv values for the GPR model
+        X = self.acidequiv.reshape(-1, 1)  # Reshape the acid equiv values for the GPR model
         if dset == 'yield':
             Y = [self.yieldvalues]
         elif dset == 'conv':
@@ -127,12 +120,12 @@ class Halogenation:
         else:
             raise ValueError("dset must be either 'yield', 'conv' or 'both'")
 
-        #run the GPR model
-        model = GaussianProcessRegressor(kernel = kernel, n_restarts_optimizer = 7) #define the GPR model
-        pipe = Pipeline([('scaler', scaler), ('model', model)]) #define the pipeline (scaler + model)
-        x = np.linspace(np.min(X.flatten()), np.max(X.flatten()), 1000).reshape(-1, 1) #define the x values for the prediction. 1000 points are used between the min and max acid equiv values
+        # Build and run the GPR model
+        model = GaussianProcessRegressor(kernel = kernel, n_restarts_optimizer = 7) # Define the GPR model
+        pipe = Pipeline([('scaler', scaler), ('model', model)]) # Define the pipeline (scaler + model)
+        x = np.linspace(np.min(X.flatten()), np.max(X.flatten()), 1000).reshape(-1, 1) # Define the x values for the prediction. 1000 points are used between the min and max acid equiv values
 
-        for i, y in enumerate(Y): #loop through the datasets (yield and conversion)
+        for i, y in enumerate(Y): # Loop through yield and conversion for the halogenation reaction data
             pipe.fit(X, y) #fit the model to yield/ conversion data
         
             y_pred, y_pred_std = pipe.predict(x, return_std=True) #predict the yield/ conversion values for all 1000 x values with standard deviation for each datapoint
@@ -140,32 +133,30 @@ class Halogenation:
             model_pred = np.column_stack((x, y_pred))
             model_stdev = np.column_stack((x, y_pred_std))
 
-            #find the maxima
-            peak_index, _ = find_peaks(model_pred[:,1].ravel(), height = height) #use some signal processing to find the peaks in the acid equiv vs yield curve above the specified height. Returns indexes of the maxima
-            indexs = [0] + list(peak_index) + [-1] #add the min and max datapoints also, for cases where the dataset has no maxima (optimum at min or max acid equiv)
+            # Peak-picking
+            peak_index, _ = find_peaks(model_pred[:,1].ravel(), height = height) # Use signal processing to find the peaks in the acid equiv vs yield curve above the specified height. Returns indexes of the maxima
+            indexs = [0] + list(peak_index) + [-1] # Add the min and max datapoints also, for cases where the dataset has no maxima (i.e optimum at min or max acid equiv)
 
-            maxima = model_pred[indexs] #find the acid equiv and yield values at the indexes of maxima
+            maxima = model_pred[indexs] # Find the acid equiv and yield values at the indexes of maxima
 
 
-            #perfrom cross validation on each model
-            loo = LeaveOneOut() 
+            # Cross validation
+            loo = LeaveOneOut() # Use LeaveOneOut cross validation to calculate the mean absolute error for each model
+            neg_aes = [] #initialize a list to store negative mean and actual errors
 
-            #initialize a list to store negative mean and actual errors
-            neg_aes = []
-
-            #loop through the LeaveOneOut splits
+            # Loop through the LeaveOneOut splits
             for train_index, test_index in loo.split(X):
                 X_train, X_test = X[train_index], X[test_index]
                 y_train, y_test = y[train_index], y[test_index]
 
-                pipe.fit(X_train, y_train) #run a new model for all LOO splits
-                abs_error = -abs(pipe.predict(X_test) - y_test)
-                neg_aes.append(abs_error[0])
+                pipe.fit(X_train, y_train) # Run a new model for all LOO splits
+                abs_error = -abs(pipe.predict(X_test) - y_test) # Calculate the absolute error for each LOO split
+                neg_aes.append(abs_error[0]) # Append the negative absolute error to the list
             
-            #calculate the mean absolute error for all LOO split models
+            # Calculate the mean absolute error for all LOO split models
             mae = -mean(neg_aes)
 
-
+            # Store the model predictions, standard deviations, maxima, and mean absolute error in dictionaries
             if (dset == 'yield' or dset == 'both') and i == 0:
                 self.yieldoutputs.update({kernelname : {'prediction': model_pred, 'stdevs': model_stdev, 'maxima': maxima, 'mae': mae}})
             else:
@@ -191,25 +182,26 @@ class Halogenation:
                     **kwargs,
                     ):
         
-        #ensure the GPR model has been run before picking the optimum conditions
+        # Ensure the GPR model has been run before picking the optimum conditions
         try:
             yield_kernel = kwargs.get('yield_kernel', list(self.yieldoutputs.keys())[0])
-            conv_kernel = kwargs.get('conv_kernel', list(self.convoutputs.keys())[0])
+        except Exception: 
+            try:
+                conv_kernel = kwargs.get('conv_kernel', list(self.convoutputs.keys())[0])
+            except Exception:
+                raise ValueError("Use self.gprcalculate() first to generate predictions") #raise an error if the GPR model has not been run
         
-        except:
-            raise ValueError("Run .gprcalculate() first to generate predictions") #raise an error if the GPR model has not been run
         
+        self.optimum = self.yieldoutputs[yield_kernel]['maxima'][0] # Initialise the optimum value as the first maxima (ie the min acid equiv value)
+        for i in range(len(self.yieldoutputs[yield_kernel]['maxima'])-1): # Loop through the maxima found in the acid/yield curve
+            if self.optimum[1] + 5 < self.yieldoutputs[yield_kernel]['maxima'][i+1][1]: # For each maxima at increasing TFA quantities, if there is a significant increase in yield (+5%) for subsiquent maxima.... 
+                self.optimum = self.yieldoutputs[yield_kernel]['maxima'][i+1] # Chainge the optimum value to the acid equiv value at this maxima
         
-        self.optimum = self.yieldoutputs[yield_kernel]['maxima'][0] #initialise the optimum value as the first maxima (ie the min acid equiv value)
-        for i in range(len(self.yieldoutputs[yield_kernel]['maxima'])-1): #loop through the maxima found in the acid/yield curve
-            if self.optimum[1] + 5 < self.yieldoutputs[yield_kernel]['maxima'][i+1][1]: #if there is a significant increase in yield (+5%) for subsiquent maxima.... 
-                self.optimum = self.yieldoutputs[yield_kernel]['maxima'][i+1] #chainge the optimum value to the acid equiv value at this maxima
-        
-        #find corresponding conversion to the YIELD maxima
+        # Find corresponding conversion to the YIELD maxima
         if self.convoutputs[conv_kernel] != None:
             convindex = np.where(self.yieldoutputs[yield_kernel]['prediction'] == self.optimum)
             conv = self.convoutputs[conv_kernel]['prediction'][convindex[0]][0][-1]
             self.optimum = np.append(self.optimum, conv)
         
-        return self.optimum #return the optimum acid equiv with predicted yield and conversion values
+        return self.optimum # Return the optimum acid equiv with predicted yield and conversion values
 
